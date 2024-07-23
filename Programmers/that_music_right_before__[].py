32794578d86036b08941568b1a601f627c6c2775
jh <jh2two@gmail.com>
2024-07-24 00:09:04 +0900

    [solved]programmers_that_music

--- Programmers/that_music_right_before__[].py
+++ Programmers/that_music_right_before__[].py
@@ -3,19 +3,19 @@ def solution(m, musicinfos):
     answer = ''
 
     # 방금그곡 서비스에서는 음악 제목, 재생이 시작되고 끝난 시각, 악보를 제공한다.
-    # 네오가 기억한 멜로디와 악보에 사용되는 음은 C, C#, D, D#, E, F, F#, G, G#, A, A#, B 12개이다.
+    # 네오가 기억한 멜로디와 악보에 사용되는 음은 C, C#, D, D#, E, F, F#, G, G#, A, A#, B, B# 12개이다.
     # 각 음은 1분에 1개씩 재생된다. 음악은 반드시 처음부터 재생되며 음악 길이보다 재생된 시간이 길 때는 음악이 끊김 없이 처음부터 반복해서 재생된다. 음악 길이보다 재생된 시간이 짧을 때는 처음부터 재생 시간만큼만 재생된다.
     # 음악이 00:00를 넘겨서까지 재생되는 일은 없다.
     # 조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다. 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
     # 조건이 일치하는 음악이 없을 때에는 “(None)”을 반환한다.
 
-    target_melody = m.replace("C#", 'H')
+    target_melody = m.replace("C#", 'c').replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a").replace("B#", 'b')
     longest_played_time = 0
     curr_best_music = "(None)"
     for info_str in musicinfos:
         m_info = info_str.split(',')
         m_title = m_info[2]
-        m_info[3] = m_info[3].replace("C#", "H")
+        m_info[3] = m_info[3].replace("C#", 'c').replace("D#", "d").replace("F#", "f").replace("G#", "g").replace("A#", "a").replace("B#", 'b')
         m_len = len(m_info[3])
 
         # 재생 시간을 초 단위로 계싼
@@ -42,5 +42,6 @@ def solution(m, musicinfos):
     return curr_best_music
 
 # print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
-print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
+# print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
 # print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("ABC#A", ["04:00,04:03,NAME,ABC#D"]))
