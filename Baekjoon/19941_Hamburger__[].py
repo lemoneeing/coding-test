f96ea1b6b7195c600c6a6fe804872769b223ab68
kjh <jh2two@gmail.com>
2024-10-15 20:55:07 +0900

    [solved] Baek_Hamburger

--- Baekjoon/19941_Hamburger__[].py
+++ Baekjoon/19941_Hamburger__[].py
@@ -1,25 +1,48 @@
 import sys
 
 # 시간 초과
+# def solution():
+    # ans = 0
+    
+    # N, K = map(int, sys.stdin.readline().strip().split())
+    # arr = list(sys.stdin.readline().strip())
+    # ham = []
+    # per = []
+    # for i, c in enumerate(arr):
+    #     if c == 'H':
+    #        ham.append(i)
+    #     elif c == 'P':
+    #         per.append(i)
+            
+    # for p in per:
+    #     del_target = []
+    #     for h in ham:
+    #         if p-K <= h <= p+K:
+    #             ans+=1
+    #             del_target.append(h)
+    #             break
+    #         if h < p:
+    #             del_target.append(h)
+    #     for dt in del_target:
+    #         ham.remove(dt)
+    
+    # sys.stdout.write(f"{ans}")
+
 def solution():
     ans = 0
     
     N, K = map(int, sys.stdin.readline().strip().split())
     arr = list(sys.stdin.readline().strip())
-    ham = []
-    per = []
-    for i, c in enumerate(arr):
-        if c == 'H':
-           ham.append(i)
-        elif c == 'P':
-            per.append(i)
-            
-    for p in per:
-        for h in ham:
-            if p-K <= h <= p+K:
-                ans+=1
-                ham.remove(h)
-                break
-    
+    for p, c in enumerate(arr):
+        if c == 'P':
+            s = p - K if p >= K else 0
+            e = p + K if p < N - K else N - 1
+            for h in range(s, e+1):
+                if arr[h] == 'H':
+                    ans += 1
+                    arr[h] = ''
+                    break
+                
     sys.stdout.write(f"{ans}")
+    
 solution()
