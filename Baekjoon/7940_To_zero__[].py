32f9e874133c709835c56801ec94691f7642df0b
kjh <jh2two@gmail.com>
2024-10-16 20:11:51 +0900

    [solving] Baek_To_zero__(mess!)

--- Baekjoon/7940_To_zero__[].py
+++ Baekjoon/7940_To_zero__[].py
@@ -3,51 +3,36 @@ import sys
 def solution():
     def BT(res, operand, limit, exp):
         
-        if operand > limit:
+        if len(exp) == limit:
             if res == 0:
                 sys.stdout.write(f"1")
-                for i, op in enumerate(exp):
+                for i, op in enumerate(exp[1:]):
                     sys.stdout.write(f"{op}{i+2}")
                 sys.stdout.write(f"\n")
                 return
             else:
                 return
-        
+
+        if len(exp) > 0:
+            # 공백
             exp.append(' ')
-            BT(int(f"1{str(operand)}"), operand+1, limit, exp)
+            BT(res, operand*10+operand+1, limit, exp)
             exp.pop()
-        if res == 1:
-            # 1 다음에 공백
-        
-        # '+', 공백
-        exp.append('+')
-        exp.append(' ')
-        BT(res+int(f"{str(operand)}"f"{str(operand+1)}"), operand+2, limit, exp)
-        exp.pop()
-        exp.pop()
         
         # 연산자 '+'
         exp.append('+')
         BT(res+operand, operand+1, limit, exp)
         exp.pop()
         
-        # '-', 공백
-        exp.append('-')
-        exp.append(' ')
-        BT(res-int(f"{str(operand)}"f"{str(operand+1)}"), operand+2, limit, exp)
-        exp.pop()
-        exp.pop()
-        
-        # 연산자 '-'
-        exp.append('-')
-        BT(res-operand, operand+1, limit, exp)
-        exp.pop()
+        if len(exp) > 0:
+            # 연산자 '-'
+            exp.append('-')
+            BT(res-operand, operand+1, limit, exp)
+            exp.pop()
     
     C = int(sys.stdin.readline().strip())
-    for _ in range(C):
-        N = int(sys.stdin.readline().strip())
-        
-        BT(1, 2, N, [])
+    for _ in range(C):        
+        BT(0, 1, int(sys.stdin.readline().strip()), [])
         sys.stdout.write(f"\n")
         
 solution()
