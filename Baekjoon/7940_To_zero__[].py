95707b2fbcd65607888356b861dfb23561ca19d4
kjh <jh2two@gmail.com>
2024-10-16 19:38:19 +0900

    [solvine] Baek_7940__[Backtraking]__(whywrong?)

--- Baekjoon/7940_To_zero__[].py
+++ Baekjoon/7940_To_zero__[].py
@@ -1,34 +1,53 @@
 import sys
 
 def solution():
+    def BT(res, operand, limit, exp):
+        
+        if operand > limit:
+            if res == 0:
+                sys.stdout.write(f"1")
+                for i, op in enumerate(exp):
+                    sys.stdout.write(f"{op}{i+2}")
+                sys.stdout.write(f"\n")
+                return
+            else:
+                return
+        
+            exp.append(' ')
+            BT(int(f"1{str(operand)}"), operand+1, limit, exp)
+            exp.pop()
+        if res == 1:
+            # 1 다음에 공백
+        
+        # '+', 공백
+        exp.append('+')
+        exp.append(' ')
+        BT(res+int(f"{str(operand)}"f"{str(operand+1)}"), operand+2, limit, exp)
+        exp.pop()
+        exp.pop()
+        
+        # 연산자 '+'
+        exp.append('+')
+        BT(res+operand, operand+1, limit, exp)
+        exp.pop()
+        
+        # '-', 공백
+        exp.append('-')
+        exp.append(' ')
+        BT(res-int(f"{str(operand)}"f"{str(operand+1)}"), operand+2, limit, exp)
+        exp.pop()
+        exp.pop()
+        
+        # 연산자 '-'
+        exp.append('-')
+        BT(res-operand, operand+1, limit, exp)
+        exp.pop()
     
-    # # 문제 잘못 이해: 1 ~ N 까지만 사용하는 줄
-    # def BT(res, operand, limit, exp):
+    C = int(sys.stdin.readline().strip())
+    for _ in range(C):
+        N = int(sys.stdin.readline().strip())
         
-    #     if operand > limit:
-    #         if res == 0:
-    #             sys.stdout.write(f"1")
-    #             for i, op in enumerate(exp):
-    #                 sys.stdout.write(f"{op}{i+2}")
-    #             sys.stdout.write(f"\n")
-    #             return
-    #         else:
-    #             return
+        BT(1, 2, N, [])
+        sys.stdout.write(f"\n")
         
-    #     # 연산자 '+'
-    #     exp.append('+')
-    #     BT(res+operand, operand+1, limit, exp)
-    #     exp.pop()
-        
-    #     # 연산자 '-'
-    #     exp.append('-')
-    #     BT(res-operand, operand+1, limit, exp)
-    #     exp.pop()
-        
-    
-    # C = int(sys.stdin.readline().strip())
-    # for _ in range(C):
-    #     N = int(sys.stdin.readline().strip())
-    #     BT(1, 2, N, [])
-    #     sys.stdout.write(f"\n")
 solution()
