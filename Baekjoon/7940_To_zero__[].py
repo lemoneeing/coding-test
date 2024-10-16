bb19c3d7e34934c0507d73bd9b1dac8ccc526a7b
kjh <jh2two@gmail.com>
2024-10-16 19:13:05 +0900

    [solving] Baek_To_zero__[Backtracking]...misunderstaing

--- Baekjoon/7940_To_zero__[].py
+++ Baekjoon/7940_To_zero__[].py
@@ -2,30 +2,33 @@ import sys
 
 def solution():
     
-    def bt(res, operand, limit, exp):
-        if operand > limit:
-            return res
+    # # 문제 잘못 이해: 1 ~ N 까지만 사용하는 줄
+    # def BT(res, operand, limit, exp):
         
-        # 연산자 '+'
-        if bt(res+operand, operand+1, True, limit) == 0:
-            exp.append('+') 
+    #     if operand > limit:
+    #         if res == 0:
+    #             sys.stdout.write(f"1")
+    #             for i, op in enumerate(exp):
+    #                 sys.stdout.write(f"{op}{i+2}")
+    #             sys.stdout.write(f"\n")
+    #             return
+    #         else:
+    #             return
         
-        # 연산자 '-'
-        if bt(res-operand, operand+1, False, limit) == 0:
-            exp.append('-')
+    #     # 연산자 '+'
+    #     exp.append('+')
+    #     BT(res+operand, operand+1, limit, exp)
+    #     exp.pop()
         
+    #     # 연산자 '-'
+    #     exp.append('-')
+    #     BT(res-operand, operand+1, limit, exp)
+    #     exp.pop()
         
-    ans = 0
     
-    C = int(sys.stdin.readline().strip())
-    for _ in range(C):
-        calc = 0
-        ans = []
-        N = int(sys.stdin.readline().strip())
-        bt(0, 1, N, ans)
-        
-        sys.stdout.write(f"1")
-        for i in range(2, N+1):
-            sys.stdout.write(f"{ans.pop()}{i}")
-        
+    # C = int(sys.stdin.readline().strip())
+    # for _ in range(C):
+    #     N = int(sys.stdin.readline().strip())
+    #     BT(1, 2, N, [])
+    #     sys.stdout.write(f"\n")
 solution()
