6cf21e3b2316a5f51cd8c9eb67a057dc6d243ce9
jh <jh2two@gmail.com>
2024-10-24 23:26:41 +0900

    [solving]Baek_light_bulbs__(nbm)

--- Baekjoon/2138_light_bulb_and_switch__[].py
+++ Baekjoon/2138_light_bulb_and_switch__[].py
@@ -1,43 +1,43 @@
 import sys
+
 sys.setrecursionlimit(10 ** 6)
+
 
 def solution():
     N = int(sys.stdin.readline().strip())
     bulbs = list(sys.stdin.readline().strip())
     quiz = sys.stdin.readline().strip()
-    neighbor = [-1, 0, 1]
 
-    def bt(curr_bulbs, order, click):
+    def switch(curr_bulbs, count):
         nonlocal quiz, N
-        if ''.join(curr_bulbs) == quiz:
-            return click
 
-        elif order == N:
-            return -1
+        for i in range(N):
+            if curr_bulbs[i] != quiz.__getitem__(i):
+                count += 1
+                for n in [-1, 0, 1]:
+                    curr = i+n
+                    if 0 <= curr < N:
+                        curr_bulbs[curr] = str(1 - int(curr_bulbs[curr]))
 
-        no_click = bt(curr_bulbs, order + 1, click)
+            if ''.join(curr_bulbs) == quiz:
+                return count
+        return -1
 
-        for n in neighbor:
-            curr = order + n
-            if 0 <= curr < N:
-                curr_bulbs[curr] = str(1 - int(curr_bulbs[curr]))
 
-        yes_click = bt(curr_bulbs, order + 1, click + 1)
+    keep_first = switch(bulbs.copy(), 0)
+    bulbs[0] = str(1 - int(bulbs[0]))
+    change_first = switch(bulbs.copy(), 1)
 
-        for n in neighbor:
-            curr = order + n
-            if 0 <= curr < N:
-                curr_bulbs[curr] = str(1 - int(curr_bulbs[curr]))
+    if keep_first < 0 and change_first < 0:
+        ans = -1
+    elif change_first < 0:
+        ans = keep_first
+    elif keep_first < 0:
+        ans = change_first
+    else:
+        ans = min(keep_first, change_first)
 
-        if no_click < 0 and yes_click < 0:
-            return -1
-        elif yes_click < 0:
-            return no_click
-        elif no_click < 0:
-            return yes_click
-        else:
-            return min(no_click, yes_click)
+    sys.stdout.write(f"{ans}")
 
-    sys.stdout.write(f"{bt(bulbs.copy(), 0, 0)}")
 
 solution()
