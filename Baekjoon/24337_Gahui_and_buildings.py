ad60723116414bb19aeff96368e501114cacf77e
jh <jh2two@gmail.com>
2024-11-11 23:31:32 +0900

    [solving]Baek_Gahui_and_buildings

--- Baekjoon/24337_Gahui_and_buildings.py
+++ Baekjoon/24337_Gahui_and_buildings.py
@@ -7,5 +7,28 @@ def solution():
     if a == N == b:
         sys.stdout.write('-1')
 
+    ans = []
+
+    curr = 1
+    # curr < a 일 동안 ans 에 1 부터 오름차순 추가
+    while curr < a:
+        ans.append(curr)
+        if N - len(ans) < b:
+            sys.stdout.write('-1')
+            return
+        curr += 1
+
+    # a loop 완료 & N - len(ans) >= b 이면 N-len(ans)-b 만큼 0 추가
+    if N - len(ans) >= b:
+        last = ans[-1]
+        ans.extend([0]*(N-len(ans)-b))
+
+    # ans 의 0 제외 마지막 숫자와 동일한 숫자부터 1씩 내림차순 추가
+    curr = last
+    while last-curr < b:
+        ans.append(curr)
+        curr -= 1
+
+    sys.stdout.write(' '.join(list(map(str, ans))))
 
 solution()
