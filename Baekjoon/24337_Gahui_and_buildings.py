b85164a3804cc7493c3687ffb9dc4485faa92066
kjh <jh2two@gmail.com>
2024-11-18 19:06:53 +0900

    Baek_Gahui_building

--- Baekjoon/24337_Gahui_and_buildings.py
+++ Baekjoon/24337_Gahui_and_buildings.py
@@ -9,23 +9,27 @@ def solution():
 
     ans = []
 
-    curr = 1
+    curr = 0
     # curr < a 일 동안 ans 에 1 부터 오름차순 추가
     while curr < a:
+        curr += 1
         ans.append(curr)
-        if N - len(ans) < b:
+        if N - len(ans) < b - 1:
             sys.stdout.write('-1')
             return
-        curr += 1
 
+    curr = ans[-1]
+        
     # a loop 완료 & N - len(ans) >= b 이면 N-len(ans)-b 만큼 0 추가
-    if N - len(ans) >= b:
-        last = ans[-1]
-        ans.extend([0]*(N-len(ans)-b))
+    forB = N - len(ans)
+    if forB >= b:
+        ans.extend([1]*(forB-b))
 
     # ans 의 0 제외 마지막 숫자와 동일한 숫자부터 1씩 내림차순 추가
-    curr = last
-    while last-curr < b:
+    if N - len(ans) == b-1:
+        ans[-1] += 1
+        
+    while len(ans) < N:
         ans.append(curr)
         curr -= 1
 
--- Baekjoon/24337_Gahui_and_buildings__[].py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-def solution():
-    N, g, d = map(int, input().strip().split())
-    
-    print(N, g, d)
-    
-solution()
