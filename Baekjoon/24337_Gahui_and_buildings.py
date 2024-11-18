6da9b1a040462a4f032d1b43e4bae55da0d8a0de
kjh <jh2two@gmail.com>
2024-11-18 19:34:18 +0900

    Baek_Gahui_building__branching

--- Baekjoon/24337_Gahui_and_buildings.py
+++ Baekjoon/24337_Gahui_and_buildings.py
@@ -8,31 +8,41 @@ def solution():
         sys.stdout.write('-1')
 
     ans = []
-
-    curr = 0
-    # curr < a 일 동안 ans 에 1 부터 오름차순 추가
-    while curr < a:
-        curr += 1
-        ans.append(curr)
-        if N - len(ans) < b - 1:
-            sys.stdout.write('-1')
-            return
-
-    curr = ans[-1]
-        
-    # a loop 완료 & N - len(ans) >= b 이면 N-len(ans)-b 만큼 0 추가
-    forB = N - len(ans)
-    if forB >= b:
-        ans.extend([1]*(forB-b))
-
-    # ans 의 0 제외 마지막 숫자와 동일한 숫자부터 1씩 내림차순 추가
-    if N - len(ans) == b-1:
-        ans[-1] += 1
-        
+    start = 0
     while len(ans) < N:
-        ans.append(curr)
-        curr -= 1
+        forA = []
+        curr = start
+        # curr < a 일 동안 ans 에 1 부터 오름차순 추가
+        for i in range(a):
+            curr += 1
+            forA.append(curr)
+            if N - len(forA) < b - 1:
+                sys.stdout.write('-1')
+                return
+            
+        # 막빌 높이가 b 보다 작으면 b부터 거꾸로 채움.
+        if curr > b:
+            if N - len(forA) > b:
+                ans = forA + [1]*(N-a-b) + [i for i in range(1, b+1)][::-1]
+            else:
+                print(-1)
+                return
+            
+        # 막빌 높이가 b와 같으면 막빌 -1 부터 거꾸로 b-1까지 채움. b번째부터 1 채움
+        elif curr == b:
+            ans = forA + [e for e in range(curr-1, 1, -1)]
+            if len(ans) < N:
+                ans += [ans[-1]-1] * (N - len(ans))
 
-    sys.stdout.write(' '.join(list(map(str, ans))))
+        # 막빌 높이가 b보다 작으면 b - 막빌높이 만큼 더함.
+        elif curr < b:
+            forA[-1] += b-curr
+            ans = forA + [i for i in range(1, b)][::-1]
+            
+        if len(ans) == N:
+            sys.stdout.write(' '.join(list(map(str, ans))))
+            return
+        
+        start += 1
 
 solution()
