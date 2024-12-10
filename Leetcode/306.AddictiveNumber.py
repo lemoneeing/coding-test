78b1fd00e5252d0a789b658a1a2b7f03685758d1
kjh <jh2two@gmail.com>
2024-12-10 21:17:30 +0900

    [solving]Leet_AddictiveNum

--- Leetcode/306.AddictiveNumber.py
+++ Leetcode/306.AddictiveNumber.py
@@ -1,30 +1,38 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        if num.startswith('0'):
-            return False
-
-        fI = 0
-        fL = 1
-        while fL <= len(num)//2:
+        cfL = 1
+        while cfL <= len(num)//2:
+            fI = 0
+            fL = cfL
+            start = True
             sI = fI+fL
             sL = len(num[fI+fL:]) // 2
             while sL > 0:
+                if (fL > 1 and num[fI:fI+fL].startswith('0')) or (sL > 1 and num[sI:sI+sL].startswith('0')):
+                    return False
                 subSum = str(int(num[fI:fI+fL]) + int(num[sI:sI+sL]))
                 if num[sI+sL:] == subSum:
                     return True
                 else:
                     if num[sI+sL:].startswith(subSum):
+                        start = False
                         fI = sI
                         fL = sL
                         sI = sI+sL
                         sL = len(subSum)
                     else:
-                        sL -= 1
+                        if start:
+                            sL -= 1
+                        else:
+                            break
 
-            fL+=1
+            cfL+=1
             
         return False
 
-# print(Solution().isAdditiveNumber('231336497'))
-# print(Solution().isAdditiveNumber('1752227381'))
-print(Solution().isAdditiveNumber('17522274976125'))
+print(Solution().isAdditiveNumber('231336497')) #True
+print(Solution().isAdditiveNumber('1752227381')) #False
+print(Solution().isAdditiveNumber('17522274976125')) #True
+print(Solution().isAdditiveNumber('011112')) #False
+print(Solution().isAdditiveNumber('000')) # True
+print(Solution().isAdditiveNumber('12122436')) # True
