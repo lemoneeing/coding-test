d0ac44c22be7997d33e2d609d21ecd65a5d4dcec
jh <jh2two@gmail.com>
2024-12-18 23:51:52 +0900

    [solving]Baek_snake_and_ladder

--- Baekjoon/16928_Snake_and_ladder__[DP].py
+++ Baekjoon/16928_Snake_and_ladder__[DP].py
@@ -1,3 +1,4 @@
+import math
 import sys
 from heapq import heappush
 
@@ -8,15 +9,37 @@ def solution():
     path = []
     snakes = []
     ladders = []
-    for _ in range(L+S):
-        heappush(path, tuple(map(int, input().split())))
-        if path[-1][0] < path[-1][1]:
-            heappush(ladders, path[-1])
+    for _ in range(L):
+        num_pair = tuple(map(int, input().split()))
+        heappush(path, num_pair)
+        heappush(ladders, num_pair)
+
+    for _ in range(S):
+        num_pair = tuple(map(int, input().split()))
+        heappush(path, num_pair)
+        heappush(snakes, num_pair)
+
+    # 기본
+    board = [0] * 101
+    for i in range(101):
+        if i < 6:
+            board[i] = i % 6
         else:
-            heappush(snakes, path[-1])
+            board[i] = math.ceil(i / 6)
+
+    # 사다리
+    for ladder in ladders:
+        board[ladder[1]] = min(board[ladder[1]], board[ladder[0]])
+
+    # 사디리로 도달 횟수가 갱신된 칸을 기준으로 다음 칸의 도달 횟수를 +1 씩하여 갱신 -> 100번
+
+    # 뱀
+
+    # 뱀으로 갱신된 칸 기준으로 다음 칸 도달 횟수 갱신 -> 100
+
     dp = []
     print(path)
-    print(snakes)
     print(ladders)
+    print(snakes)
 
 solution()
