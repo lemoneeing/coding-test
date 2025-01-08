d289fc31810661754278268f92d8270ae6968b9b
jh <jh2two@gmail.com>
2025-01-08 20:57:35 +0900

    [solved]Baek_Go_travel!__[DFS] + 유니온파인드로도 풀어몰 것

--- Baekjoon/1976_Go_travel!__[DFS].py
+++ Baekjoon/1976_Go_travel!__[DFS].py
@@ -6,27 +6,22 @@ def solution():
     P = int(input().strip())
     path = [[]]
     for r in range(1, C+1):
-        path.append([])
-        for c, p in enumerate(list(map(int, input().strip().split()))):
-            if p == 1:
-                path[r].append(c+1)
+        path.append([0]+list(map(int, input().strip().split()))) # 인럽리스트를 사용하려 했지만, 간선이 많을 경우 인접행렬보다 메모리를 많이 사용하게 된다->메모리초과
 
     plan = []
-    visited = {}
+    visited = [False] * (C+1)
     for tg in map(int, input().strip().split()):
         plan.append(tg)
-        visited[tg] = False
 
-    ## DFS
+    ## DFS: 주어진 여행 경로들이 어떻게든 연결되어 있는지 확인하면 됨.
     stack = [plan[0]]
     while stack:
         curr = stack.pop()
-        if not visited.get(curr, False):
-            if curr in visited:
-                visited[curr] = True
-            stack.extend(path[curr])
+        if not visited[curr]:
+            visited[curr] = True
+            stack.extend([c for c, e in enumerate(path[curr]) if e == 1])
 
-        if all(visited.values()):
+        if all([v for i, v in enumerate(visited) if i in plan]):
             sys.stdout.write("YES")
             return
 
