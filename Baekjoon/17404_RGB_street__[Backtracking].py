404f449e17049523275197b3b60f6298c5a6dce4
Jeonghyun Kim <jh2two@gmail.com>
2025-03-11 23:03:35 +0900

    [input]Baek_RGB_street__[Baektracking]

---
+++ Baekjoon/17404_RGB_street__[Backtracking].py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N = int(input())
+    color_cost = []
+    for _ in range(N):
+        color_cost.append(list(map(int, input().split())))
+
+
+
+solution()
