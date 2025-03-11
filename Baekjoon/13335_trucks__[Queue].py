d5f684c6c8821ae657a82e4c381abeafc60b65eb
Jeonghyun Kim <jh2two@gmail.com>
2025-03-11 23:01:33 +0900

    [solving]Baek_trucks__[Queue]

---
+++ Baekjoon/13335_trucks__[Queue].py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    n, W, L = map(int, input().split())
+    trucks = list(map(int, input().split()))
+    brg = []
+    cost = 0
+    while trucks:
+        nxt_tr = trucks[0]
+        if sum(brg) + nxt_tr <= L:
+            brg.append(trucks.pop(0))
+            cost += 1
+        else:
+            cost += (W - len(brg))
+            brg.pop(0)
+
+        if len(brg) == W:
+            brg.pop(0)
+
+        if not trucks and brg: # 다리에 올라 가지 못한 트럭이 없음.
+            cost += W
+
+    sys.stdout.write(f"{cost}")
+
+solution()
