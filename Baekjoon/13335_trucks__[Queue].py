5a6b92ec38d74bfbc3cc4c8a40a5ae5842cfcebc
Jeonghyun Kim <jh2two@gmail.com>
2025-03-11 23:26:48 +0900

    [solving]Baek_trucks__[Queue](nbm)

--- Baekjoon/13335_trucks__[Queue].py
+++ Baekjoon/13335_trucks__[Queue].py
@@ -5,10 +5,11 @@ def solution():
     n, W, L = map(int, input().split())
     trucks = list(map(int, input().split()))
     brg = []
+    w_on_brg = 0
     cost = 0
     while trucks:
         nxt_tr = trucks[0]
-        if sum(brg) + nxt_tr <= L:
+        if w_on_brg + nxt_tr <= L:
             brg.append(trucks.pop(0))
             cost += 1
         else:
@@ -24,3 +25,35 @@ def solution():
     sys.stdout.write(f"{cost}")
 
 solution()
+
+# # GPT 풀이
+# import sys
+# from collections import deque
+#
+# input = sys.stdin.readline
+#
+# def solution():
+#     n, W, L = map(int, input().split())
+#     trucks = list(map(int, input().split()))
+#
+#     bridge = deque([0] * W)  # 다리 길이를 고려한 큐
+#     total_weight = 0  # 현재 다리 위에 있는 트럭들의 총 무게
+#     time = 0  # 경과 시간
+#
+#     while trucks or total_weight > 0:
+#         time += 1
+#
+#         # 다리 맨 앞의 트럭을 내림
+#         total_weight -= bridge.popleft()
+#
+#         if trucks:
+#             # 다음 트럭을 다리에 올릴 수 있는지 확인
+#             if total_weight + trucks[0] <= L:
+#                 truck = trucks.pop(0)
+#                 bridge.append(truck)
+#                 total_weight += truck
+#             else:
+#                 bridge.append(0)  # 트럭을 올릴 수 없으면 빈 공간 유지
+#
+#     print(time)
+# solution()
