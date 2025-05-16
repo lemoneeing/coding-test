c75f400710a0fe1e62117be444bfba7e02a86598
Jeonghyun Kim <jh2two@gmail.com>
2025-05-16 22:48:39 +0900

    [Solved]Baek_The_easy_shortest_path__[Dijk](bm)

--- Baekjoon/14940_The_easy_shortest_path__[Dijkstra].py
+++ Baekjoon/14940_The_easy_shortest_path__[Dijkstra].py
@@ -1,21 +1,51 @@
 import sys
 input = sys.stdin.readline
 
-def dijkstra(x1, y1, x2, y2, graph):
-    # graph[x1][y1] 은 start 에서 해당 지점까지의 최소 거리
-    #
-    pass
-
 def solution():
     # 모든 정점에서 2 까지의 거리 계산(2는 단 하나)
     # = 2 에서 다른 모든 정점으로까지의 거리 계산 = 다익스트라
     # 모든 정점 사이의 거리가 1로 동일하므로 2를 중심으로 가장 가까운 곳부터 탐색해야 함. = BFS
     n, m = map(int, input().split())
     board = []
+    dp = []
+    visited = []
     start = None # 2의 위치
     for r in range(n):
         row = list(map(int, input().split()))
         board.append(row)
+        dp.append([-1 for _ in range(m)])
+        visited.append([False for _ in range(m)])
         if 2 in row:
-            start = (r, row.index(2))
+            start = (r, c := row.index(2))
+            dp[r][c] = 0
+            visited[r][c] = True
 
+    dir_x = [-1, 0, 1, 0]
+    dir_y = [0, 1, 0, -1]
+
+    q = [start]
+    while q:
+        sx, sy = q.pop(0)
+        for i in range(4):
+            ex = sx + dir_x[i]
+            ey = sy + dir_y[i]
+            if 0 <= ex < n and 0 <= ey < m and not visited[ex][ey]:
+                if board[ex][ey] == 0:
+                    dp[ex][ey] = 0
+                else:
+                    if dp[ex][ey] == -1:
+                        dp[ex][ey] = min(1000001, dp[sx][sy]+1)
+                    q.append((ex, ey))
+
+                visited[ex][ey] = True
+
+    for r in range(n):
+        for c in range(m):
+            if dp[r][c] == -1 and board[r][c] == 0:
+                sys.stdout.write(f"0 ")
+            else:
+                sys.stdout.write(f"{dp[r][c]} ")
+        if r < n-1:
+            sys.stdout.write(f"\n")
+
+solution()
