6543b5ad92de7322532ee50361576dafdfbfe3e4
Jeonghyun Kim <jh2two@gmail.com>
2025-07-13 16:15:12 +0900

    [Solved]Baek_switch__trim(bm)

--- Baekjoon/1244_switch_on_off.py
+++ Baekjoon/1244_switch_on_off.py
@@ -1,15 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def solution(switch_cnt, switches, student_cnt, students):
-# def solution():
-#     switch_cnt = int(input())
-#     switches = list(map(int, input().split()))
-#
-#     student_cnt = int(input())
-#     students = []
-#     for _ in range(student_cnt):
-#         students.append(list(map(int, input().split())))
+def apply_boys_actions(actions, arr):
+    for idx, cnt in enumerate(actions):
+        if cnt % 2 != 0:
+            arr[idx] = (arr[idx] + 1) % 2
+
+def solution():
+    switch_cnt = int(input())
+    switches = list(map(int, input().split()))
+
+    student_cnt = int(input())
+    students = []
+    for _ in range(student_cnt):
+        students.append(list(map(int, input().split())))
 
     swap_cnt = [0] * switch_cnt  # 각 스위치 별로 눌러야 하는 횟수 기록
     while students:
@@ -22,29 +26,23 @@ def solution(switch_cnt, switches, student_cnt, students):
 
         else:
             if sum(swap_cnt) > 0:  # 앞서 쌓인 swap_cnt 적용
-                for idx, cnt in enumerate(swap_cnt):
-                    if cnt % 2 != 0:
-                        switches[idx] = (switches[idx] + 1) % 2
-
+                apply_boys_actions(swap_cnt, switches)
             swap_cnt = [0] * switch_cnt     # 각 스위치 별로 눌러야 하는 횟수 기록
 
             # 좌우 대칭 최대 길이
-            curr_idx = num - 1
-            offset = 1
-            longest_offset = 0
-            while 0 <= curr_idx-offset and curr_idx+offset <= switch_cnt - 1:
-                if switches[curr_idx-offset] == switches[curr_idx+offset]:
-                    longest_offset = offset
-                    offset += 1
-                else:
+            idx = num - 1
+            longest_d = 0
+            for dist in range(1, min(num, switch_cnt-num+1)):
+                if switches[idx-dist] != switches[idx+dist]: # 대칭 체크
                     break
-            for j in range(curr_idx-longest_offset, curr_idx+longest_offset+1):
+
+                longest_d = dist
+
+            for j in range(idx-longest_d, idx+longest_d+1):
                 switches[j] = (switches[j] + 1) % 2
 
     if sum(swap_cnt) > 0:  # 앞서 쌓인 swap_cnt 적용
-        for idx, cnt in enumerate(swap_cnt):
-            if cnt % 2 != 0:
-                switches[idx] = (switches[idx] + 1) % 2
+        apply_boys_actions(swap_cnt, switches)
 
     idx_in_a_line = 0
     for on_off in switches:
@@ -57,6 +55,6 @@ def solution(switch_cnt, switches, student_cnt, students):
 
         idx_in_a_line += 1
 
-# solution(45, [1 for _ in range(45)], 1, [(1, 3)])
+solution(45, [1 for _ in range(45)], 1, [(1, 3)])
 # solution(10, list(map(int, '0 1 0 0 1 1 0 0 0 0'.split())), 2, [(2, 3), (2, 4)])
-solution(10, list(map(int, '0 1 0 0 1 1 1 1 1 1'.split())), 2, [(2, 2)])
+# solution(10, list(map(int, '0 1 0 0 1 1 1 1 1 1'.split())), 2, [(2, 2)])
