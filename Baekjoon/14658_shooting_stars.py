c90f4860ee4187cf43c6b77646f2ac4c89debd1c
Jeonghyun Kim <jh2two@gmail.com>
2025-08-15 23:44:21 +0900

    [solving] Baek_shooting_stars

---
+++ Baekjoon/14658_shooting_stars.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N, M, L, K = map(int, input().split())
+    stars = []
+    for __ in range(K):
+        stars.append(list(map(int, input().split())))
+
+    for row in range(N):
+        for col in range(M):
+
+
+    ans = ''
+
+
+solution()
