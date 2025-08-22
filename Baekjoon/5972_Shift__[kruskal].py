4901dcb79ad7b956c87690c191db865a2e0f7471
Jeonghyun Kim <jh2two@gmail.com>
2025-08-23 01:07:46 +0900

    [Solved]Baek_Shift__[kruskal](nbm)

--- Baekjoon/5972_Shift__[kruskal].py
+++ Baekjoon/5972_Shift__[kruskal].py
@@ -1,41 +1,29 @@
 import sys
-from queue import PriorityQueue
+import heapq
 
 input = sys.stdin.readline
 
-
 def solution():
     N, M = map(int, input().split())
-    edges = [[] for _ in range(N+1)]
+    edges = [[] for _ in range(N + 1)] # 인접리스트
     for _ in range(M):
         u, v, w = map(int, input().split())
-        edges[u].append((w, v))
-        edges[v].append((w, u))
+        edges[u].append((v, w))
+        edges[v].append((u, w))
 
-    vertexes = [e for e in edges[1]]
-    visited = [False] * (N+1)
-    visited[1] = True
-    min_c = sys.maxsize
-    curr_c = 0
-    s = 1
-    while vertexes:
-        cost, e = vertexes[-1]
-        if not visited[e]:
-            curr_c += cost
-            vertexes.pop()
-            vertexes.extend(edges[e])
-            visited[e] = True
-            if e == N:
-                min_c = min(min_c, curr_c)
-                curr_c -= cost
-                visited[s] = False
-                visited[e] = False
-            else:
-                s = e
-        else:
-            vertexes.pop()
+    cost = [sys.maxsize] * (N + 1)
+    cost[1] = 0
+    pq = [(0, 1)] # (비용, 목적지)
+    while pq:
+        curr_cost, m = heapq.heappop(pq)    # curr_cost: 출발지(1)에서 중간지점(m)까지 다이렉트로 이동하는 비용
+                                            # m: 출발지(1)에서 가장 최소비용으로 갈 수 있는 노드.
+        if cost[m] < curr_cost:             # 아래 for문에서 pq에 미리 추가되었던 노드의 비용이 그 사이 다른 중간지점(m)을 거쳐 더 작은 비용으로 갱신되었을 경우
+            continue
+        for d, w in edges[m]:               # 출발지(1) ~ 중간지점(m) ~ 목적지(d)까지 갈 수 있는 경로(간선) 중 최소 비용 탐색
+            if cost[d] > curr_cost + w:     # cost[d]는 출발지(1) ~ 목적지(d)까지 다이렉트로 이동하는 비용
+                cost[d] = curr_cost + w
+                heapq.heappush(pq, (cost[d], d))  # 출발지에서 노드 m 을 거쳐 갈 수 있는 노드들을 힙으로 삽입. -> 그 중 가장 최소 비용인 노드부터 탐색하게 됨.(힙이니까)
 
-
-    sys.stdout.write(f"{min_c}")
+    sys.stdout.write(f"{cost[N]}")
 
 solution()
