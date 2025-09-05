a02e6d93556a6e06210c5969c04cf53fc52c13cb
Jeonghyun Kim <jh2two@gmail.com>
2025-09-05 23:46:38 +0900

    [Solved]Baek_Build_the_bridge__[BFS](nbm)

--- Baekjoon/2146_Build_the_shortest_bridge__[BFS].py
+++ Baekjoon/2146_Build_the_shortest_bridge__[BFS].py
@@ -4,10 +4,8 @@ import copy
 def solution():
     N = int(sys.stdin.readline())
     wm = []
-    # visited = []
     for _ in range(N):
         wm.append(list(map(int, sys.stdin.readline().split())))
-        # visited.append([False for __ in range(N)])
 
     dx = (0, 1, 0, -1)
     dy = (-1, 0, 1, 0)
@@ -25,19 +23,19 @@ def solution():
             visited[r][c] = True
             wm[r][c] = isl_id
             while q:
-                c_r, c_c = q.pop(0)
+                c_r, c_c = q.pop(0) # 현재 위치
                 for x, y in zip(dx, dy):
                     nx_r = c_r+y
                     nx_c = c_c+x
+                    # 범위 내 존재 + 바다가 아닌 곳 = 현재 위치와 이어진 섬
                     if 0 <= nx_r < N and 0 <= nx_c < N and not visited[nx_r][nx_c] and wm[nx_r][nx_c] != 0:
                         wm[nx_r][nx_c] = isl_id
-                        q.append((nx_r, nx_c)) # q 에는 0 보다 큰 타일의 좌표만 저장됨.
+                        q.append((nx_r, nx_c)) # 다음 탐색 위치 = 0 보다 큰 값의 좌표
                         visited[nx_r][nx_c] = True
 
             isl_id -= 1
 
     # 가장 짧은 다리 찾기 (bfs)
-    shortest_br = sys.maxsize
     shortest_dist = sys.maxsize
     for r in range(N):
         for c in range(N):
@@ -49,28 +47,30 @@ def solution():
             visited[r][c] = True
             dist = -1
             arrival = False
+            # 최초: 현재 거리(dist) 내에서 탐색 가능한 위치(1)만 저장 -> 탐색 도중: 현재 거리 내에서  탐색 가능한 위치 + 다음 거리(dist+1) 내에서 탐색 가능한 위치
             while q:
-                limit = len(q)
+                limit = len(q) # 현재 거리(dist) 내에서 탐색 가능한 위치들만 선별히기 위한 장치
                 dist += 1
+
+                # 현재 거리 내에서 탐색 가능한 위치만 pop되므로 이 loop을 탈출하면 q에 남게 되는 위치는 다음 거리 내에서 탐색 가능한 위치만 남게 됨.
                 for i in range(limit):
                     cr, cc = q.pop(0)
                     for dc, dr in zip(dx, dy):
                         nr = cr + dr
                         nc = cc + dc
 
+                        # 범위 내 존재 + 출발 섬과 같지 않은 위치
                         if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and wm[nr][nc] != wm[r][c]:
-                            if wm[nr][nc] < 0:
+                            if wm[nr][nc] < 0: # 각 섬의 id 는 음수 값
                                 arrival = True
-                                break
+                                break # 가장 빠르게 찾아낸 다른 섬이므로 더 이상 탐색 필요 없음.
                             q.append((nr, nc))
                             visited[nr][nc] = True
 
-                    if arrival:
+                    if arrival: # loop 를 한 번 더 탈출하기 위해
                         shortest_dist = min(shortest_dist, dist)
                         break
 
-
     sys.stdout.write(f"{shortest_dist}")
-    pass
 
 solution()
