ffaa18e36941a53798bc7c3131dd62a59c39f283
Jeonghyun Kim <jh2two@gmail.com>
2025-09-13 09:36:33 +0900

    [Solved] Bulbs_and_switches__[Greedy](nbm)

--- Baekjoon/2138_Bulbs_and_switches__[Greedy].py
+++ Baekjoon/2138_Bulbs_and_switches__[Greedy].py
@@ -1,11 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def switch_bulb(bulb_list, i):
-    if i > 0:
-        bulb_list[i - 1:i + 2] = [1 - e for e in bulb_list[i - 1:i + 2]]
+
+def switch_bulb(bulb_list, to_be, cnt):
+    for i in range(1, len(bulb_list)):
+        if bulb_list[i-1] != to_be[i-1]:
+            bulb_list[i - 1:i + 2] = [1 - e for e in bulb_list[i - 1:i + 2]]
+
+            cnt += 1
+
+    if bulb_list == to_be:
+        return cnt
     else:
-        bulb_list[i:i + 2] = [1 - e for e in bulb_list[i:i + 2]]
+        return -1
+
 
 def solution():
     N = int(input())
@@ -14,29 +22,20 @@ def solution():
 
     # 처음 스위치 누르고 탐색
     bulbs_switch_zero = [n for n in bulbs]
-    turn_switch_from_zero = 1
-    switch_bulb(bulbs_switch_zero, 0)
-    for i in range(1, N):
-        if bulbs_switch_zero[i-1] != to_be[i-1]:
-            switch_bulb(bulbs_switch_zero, i)
-            turn_switch_from_zero += 1
+    bulbs_switch_zero[:2] = [1-e for e in bulbs[:2]]
+    turn_switch_from_zero = switch_bulb(bulbs_switch_zero, to_be, 1)
 
     # 처음 스위치 안누르고 탐색
     bulbs_switch_one = [n for n in bulbs]
-    turn_switch_from_one = 0
-    for i in range(1, N):
-        if bulbs_switch_one[i-1] != to_be[i-1]:
-            switch_bulb(bulbs_switch_one, i)
-            turn_switch_from_one += 1
+    turn_switch_from_one = switch_bulb(bulbs_switch_one, to_be, 0)
 
-    if bulbs_switch_zero == to_be and bulbs_switch_one == to_be:
+    if turn_switch_from_zero >= 0 and turn_switch_from_one >= 0:
         sys.stdout.write(f"{min(turn_switch_from_zero, turn_switch_from_one)}")
     else:
-        if bulbs_switch_zero != to_be and bulbs_switch_one == to_be:
-            sys.stdout.write(f"{turn_switch_from_one}")
-        elif bulbs_switch_zero == to_be and bulbs_switch_one != to_be:
-            sys.stdout.write(f"{turn_switch_from_zero}")
-        else:
+        if turn_switch_from_zero < 0 and turn_switch_from_one < 0:
             sys.stdout.write(f"-1")
+        else:
+            sys.stdout.write(f"{turn_switch_from_zero if turn_switch_from_zero >= 0 else turn_switch_from_one}")
+
 
 solution()
