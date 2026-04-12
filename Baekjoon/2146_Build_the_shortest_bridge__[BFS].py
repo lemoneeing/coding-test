1ee5a488df5abd48259be4b814547fa2d22e59bb
Jeonghyun Kim <jh2two@gmail.com>
2026-04-12 17:03:52 +0900

    [refactoring]Baek_Bridge__[BFS]

--- Baekjoon/2146_Build_the_shortest_bridge__[BFS].py
+++ Baekjoon/2146_Build_the_shortest_bridge__[BFS].py
@@ -1,37 +1,32 @@
 import sys
-import copy
 
+DIR = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 def solution():
     N = int(sys.stdin.readline())
     wm = []
     for _ in range(N):
         wm.append(list(map(int, sys.stdin.readline().split())))
 
-    dx = (0, 1, 0, -1)
-    dy = (-1, 0, 1, 0)
-
     # 각 섬별로 그룹핑(bfs)
-    isl_id = -1
-    visited = [[False]*N for _ in range(N)]
+    isl_id = -1 # 각 섬의 id 를 음수로 표현(섬, 바다가 양의 정수로 입력되므로) -> wm 을 바로 업데이트하므로 visited 따로 필요 없음.
     for r in range(N):
         for c in range(N):
-            if wm[r][c] <= 0:
+            if wm[r][c] <= 0: # 이미 섬으로 그루핑된 지역 탐색은 건너뛰기
                 continue
 
             # 탐색 출발점 초기화
             q = [(r, c)]
-            visited[r][c] = True
             wm[r][c] = isl_id
             while q:
-                c_r, c_c = q.pop(0) # 현재 위치
-                for x, y in zip(dx, dy):
-                    nx_r = c_r+y
-                    nx_c = c_c+x
-                    # 범위 내 존재 + 바다가 아닌 곳 = 현재 위치와 이어진 섬
-                    if 0 <= nx_r < N and 0 <= nx_c < N and not visited[nx_r][nx_c] and wm[nx_r][nx_c] != 0:
-                        wm[nx_r][nx_c] = isl_id
-                        q.append((nx_r, nx_c)) # 다음 탐색 위치 = 0 보다 큰 값의 좌표
-                        visited[nx_r][nx_c] = True
+                cr, cc = q.pop(0) # 현재 위치
+                for dr, dc in DIR:
+                    nr = cr+dc
+                    nc = cc+dr
+                    
+                    # 처음 방문하는 섬만 그루핑
+                    if 0 <= nr < N and 0 <= nc < N and wm[nr][nc] > 0:
+                        wm[nr][nc] = isl_id
+                        q.append((nr, nc)) # 현재 위치와 인접하면서 값이 0보다 큰 좌표만 push 하므로 자연스럽게 인접한 섬만 그루핑할 수 있음.
 
             isl_id -= 1
 
@@ -39,36 +34,40 @@ def solution():
     shortest_dist = sys.maxsize
     for r in range(N):
         for c in range(N):
-            if wm[r][c] == 0:
+            if wm[r][c] == 0: # 바다인 지역 스킵
                 continue
 
             q = [(r, c)]
             visited = [[False]*N for _ in range(N)]
             visited[r][c] = True
-            dist = -1
+            dist = 0
             arrival = False
             # 최초: 현재 거리(dist) 내에서 탐색 가능한 위치(1)만 저장 -> 탐색 도중: 현재 거리 내에서  탐색 가능한 위치 + 다음 거리(dist+1) 내에서 탐색 가능한 위치
             while q:
-                limit = len(q) # 현재 거리(dist) 내에서 탐색 가능한 위치들만 선별히기 위한 장치
                 dist += 1
 
                 # 현재 거리 내에서 탐색 가능한 위치만 pop되므로 이 loop을 탈출하면 q에 남게 되는 위치는 다음 거리 내에서 탐색 가능한 위치만 남게 됨.
-                for i in range(limit):
+                for _ in range(len(q)):
                     cr, cc = q.pop(0)
-                    for dc, dr in zip(dx, dy):
+                    for dc, dr in DIR:
                         nr = cr + dr
                         nc = cc + dc
 
-                        # 범위 내 존재 + 출발 섬과 같지 않은 위치
+                        # 현재 섬과 다른 영역인지 확인
                         if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and wm[nr][nc] != wm[r][c]:
-                            if wm[nr][nc] < 0: # 각 섬의 id 는 음수 값
+                            
+                            # 다음 탐색 지점이 다른 섬일 경우
+                            if wm[nr][nr] < 0:
                                 arrival = True
-                                break # 가장 빠르게 찾아낸 다른 섬이므로 더 이상 탐색 필요 없음.
-                            q.append((nr, nc))
-                            visited[nr][nc] = True
+                                shortest_dist = min(shortest_dist, dist)
+                                break
+
+                            # 바다일 경우
+                            else:
+                                q.append((nr, nc))
+                                visited[nr][nc] = True
 
                     if arrival: # loop 를 한 번 더 탈출하기 위해
-                        shortest_dist = min(shortest_dist, dist)
                         break
 
     sys.stdout.write(f"{shortest_dist}")
